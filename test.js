9281077af48063d932519b2de9382ad46563c8eb
pemrouz <p3pedram@hotmail.com>
2015-06-14 20:49:06 +0100

    do not run in browser

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var expect = require('chai').expect
+  , client = require('client')
   , file = require('./')
   
+if (client) return
+
 describe('file', function() {
 
   it('should read file', function(){
